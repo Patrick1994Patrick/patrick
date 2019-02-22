82a122e6771b6ccbc1138a830027bae24a83132f
Patrick1994Patrick <768424731@qq.com>
2019-02-22 21:33:50 +0800

    mp_pl

---
+++ multiprocess_pool.py
@@ -0,0 +1,15 @@
+import multiprocessing as mp
+
+def mathmping(a):
+
+	print('now run %s' %a)
+		
+def multiping():
+	pool = mp.Pool()
+	for i in range(10):
+		pool.apply_async(mathmping, args = (i,))
+
+	pool.close()
+	pool.join()
+if __name__ == '__main__': 
+	multiping()
